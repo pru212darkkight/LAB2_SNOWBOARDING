6f4d1091477d429ea5f894323da32fe92d7e515c
HauHuynh2k4 <ngochau1310@gmail.com>
2025-06-22 21:39:17 +0700

    pull main

--- Assets/Scrips/ScoreItem.cs
+++ Assets/Scrips/ScoreItem.cs
@@ -1,13 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
 
 public class ScoreItem : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI spinBonusText;
+    private Coroutine spinBonusCoroutine;
+    private int bonus = 100;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             AudioManager.Instance.PlaySFX(AudioManager.Instance.getStars);
-            ScoreManager.Instance.AddScore(100f);
+            ScoreManager.Instance.AddScore(bonus);
+            ScoreManager.Instance.ShowSpinBonus(bonus);
             Destroy(gameObject); 
         }
     }
